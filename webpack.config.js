ed256f1ad225a21b7bc5a1b4022514530b1de8fb
Andres Zapata <azapata@uc.edu.co>
2021-04-01 20:23:52 -0500

    se configura webpack para trasnpilar a un dist para produccion

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
-
-module.exports={
-
-    entry:'./src/main.js',
-    output:{
-        path: __dirname+'/dist' ,
-        filename: 'bundle.js'
-
-    },
-    plugins:[
-        new HTMLWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
-
-}
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
